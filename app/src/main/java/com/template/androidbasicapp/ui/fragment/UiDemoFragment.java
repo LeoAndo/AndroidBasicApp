d0f6d5d881e9673564fdf04bc57a3857c93633bf
ando <leo81@outlook.jp>
2023-11-16 10:53:20 +0900

    #39 のFragemntライフサイクルへの説明

--- app/src/main/java/com/template/androidbasicapp/ui/fragment/UiDemoFragment.java
+++ app/src/main/java/com/template/androidbasicapp/ui/fragment/UiDemoFragment.java
@@ -8,25 +8,44 @@ import androidx.fragment.app.Fragment;
 
 import android.view.View;
 
+import com.template.androidbasicapp.AppLogger;
 import com.template.androidbasicapp.R;
 import com.template.androidbasicapp.databinding.FragmentUiDemoBinding;
 
 public class UiDemoFragment extends Fragment {
+    private static final String TAG = "UiDemoFragment";
     private FragmentUiDemoBinding binding;
 
     public UiDemoFragment() {
         super(R.layout.fragment_ui_demo);
     }
 
+    /**
+     * ActivityのonCreateメソッドで処理するようなコードはここに書く
+     */
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        AppLogger.d("IN");
         binding = FragmentUiDemoBinding.bind(view);
     }
 
+    /**
+     * ActivityのonPauseメソッドで処理するようなコードはここに書く
+     */
+    @Override
+    public void onPause() {
+        super.onPause();
+        AppLogger.d("IN");
+    }
+
+    /**
+     * ActivityのonDestroyメソッドで処理するようなコードはここに書く
+     */
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+        AppLogger.d("IN");
         binding = null;
     }
 }
