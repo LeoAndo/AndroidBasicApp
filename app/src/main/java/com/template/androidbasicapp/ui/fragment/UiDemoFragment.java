b006353aff20e272ab76f4edc794e5c7875be9b0
ando <leo81@outlook.jp>
2023-12-04 19:45:16 +0900

    RecyclerViewを使ったサンプル

--- app/src/main/java/com/template/androidbasicapp/ui/fragment/UiDemoFragment.java
+++ app/src/main/java/com/template/androidbasicapp/ui/fragment/UiDemoFragment.java
@@ -6,12 +6,22 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.os.SystemClock;
 import android.view.View;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.template.androidbasicapp.BuildConfig;
 import com.template.androidbasicapp.R;
+import com.template.androidbasicapp.data.Item;
 import com.template.androidbasicapp.databinding.FragmentUiDemoBinding;
+import com.template.androidbasicapp.ui.widget.MyListAdapter;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class UiDemoFragment extends Fragment {
     private FragmentUiDemoBinding binding;
@@ -39,6 +49,31 @@ public class UiDemoFragment extends Fragment {
         } else {
             binding.map.setVisibility(View.INVISIBLE);
         }
+
+        // リストの処理 -START
+        final List<Item> items = new ArrayList<>();
+        for (int i = 1; i < 29; i++) {
+            items.add(new Item("鉄人" + i + "号"));
+        }
+        final MyListAdapter adapter = new MyListAdapter(items);
+        binding.listHorizontal.setAdapter(adapter);
+        // 縦向きの場合 -> LinearLayoutManager.VERTICALを指定する
+        binding.listHorizontal.setLayoutManager(new LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false));
+        adapter.setOnItemClickListener(item -> {
+            Snackbar.make(binding.getRoot(), item.getTitle(), Snackbar.LENGTH_LONG).show();
+        });
+
+        binding.insert.setOnClickListener(v -> {
+            final String title = "鉄人" + SystemClock.elapsedRealtime() + "号";
+            adapter.insertItem(0, new Item(title));
+        });
+        binding.update.setOnClickListener(v -> {
+            adapter.updateItem(0);
+        });
+        binding.remove.setOnClickListener(v -> {
+            adapter.removeItem(0);
+        });
+        // リストの処理 -END
     }
 
     /**
